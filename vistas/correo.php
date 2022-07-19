f79807edf5c2497bad7af314631dd8471be46829
CHRIS10092 <koriche001@gmail.com>
2022-07-18 21:34:19 -0500

    okcoo

--- vistas/correo.php
+++ vistas/correo.php
@@ -1,15 +1,66 @@
 <?php
 session_start();
- require_once '../contenido/head.php' ?>
+
+require_once "../contenido/head.php"?>
+
+<div class="row">
+
+        <label>ESCOGER USUARIO</label>
+
+</div>
+<br>
+<br>
+
+
+
+
+
+
+<?php require_once "../contenido/foot.php"?>
 <?php
-print "<p>Comienzo</p>\n";
-for ($i = 1; $i = 3; $i++) {
-print $i;    
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+$mensaje = "hola como estas es una prueba";
+require "../contenido/librerias/PHPMailer/Exception.php";
+require "../contenido/librerias/PHPMailer/SMTP.php";
+require "../contenido/librerias/PHPMailer/PHPMailer.php";
+
+$mail = new PHPMailer(true);
+
+try {
+
+    //Server settings
+    $mail->SMTPDebug = 0; // Enable verbose debug output
+    $mail->isSMTP(); // Set mailer to use SMTP
+    $mail->Host       = 'mail.femavi.com.ec'; // Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true; // Enable SMTP authentication
+    $mail->Username   = 'correo@femavi.com.ec'; // SMTP username
+    $mail->Password   = ';+k7x(QWmJAL'; // SMTP password
+    $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587; // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('correo@femavi.com.ec', 'M');
+    $mail->addAddress('koriche001@gmail.com', 'Christopher'); // Add a recipient
+    $mail->addAddress('alexsalguero1999.al@gmail.com', 'Christopher'); // Add a recipient
+    
+
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->Subject = 'datos de prueba';
+    $mail->Body    = 'ejemplo de sistema mascota urbana';
+
+    $mail->send();
+    echo 'el message se envio correctamente';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
+//echo $factura->registrar_factura();
 
 ?>
-
-
-
-<?php require_once '../template/modulos/foot.php' ?>
